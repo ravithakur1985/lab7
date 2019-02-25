e628340d736347342e7ba105e9d2f810f40c3d4d
ravithakur1985 <ravichawhan.thakur@gmail.com>
2019-02-24 18:08:45 -0800

    lab7 initial check in

---
+++ main.c
@@ -0,0 +1,31 @@
+///////////////////////////////////////
+// Lab4 main.c                       //
+// Author: Ravi Thakur               //
+// Version 1.0                       //
+//                                   //
+///////////////////////////////////////
+
+#include "stm32f30x.h"
+#include "stm32f3_Exec.h"
+#include "stm32f3_LEDS.h"
+#include "stm32f3_Button.h"
+#include "stm32f3_GeneralTImer2.h"
+#include "stm32f3_UART.h"
+#include "cli.h"
+
+
+int main (void){
+	
+	// running the init commands
+	LEDS_Init();
+	Exec_Init();	
+	Button_Init();
+	TIM2_Init();
+	UART_Init();
+	CLI_Init();
+	__enable_irq();	
+	
+	// starting the executive
+	Exec_Start();	
+}
+
